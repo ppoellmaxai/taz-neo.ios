0253ac3a0d52479936f051c93309fb46cc7fecca
Philo.Pöllmann <philo.pollmann@MacBook-Pro-2.fritz.box>
2021-01-27 12:39:11 +0100

    adjusted and expanded test ui shell script

--- taz.neo.UITests/test_ui.sh
+++ taz.neo.UITests/test_ui.sh
@@ -1,3 +1,34 @@
+#!/bin/bash
+
+jq necessary to read config
+if ! jq --version &> /dev/null
+then
+    if [[ "$OSTYPE" == "darwin"* ]]; then brew install jq
+    else echo "xcodebuild is only supported on Mac OS"
+    fi
+fi
+
+platform='test'
+config_file="./device_settings.json"
+platform="$( jq -r '.platform' $config_file )"
+device_name="$( jq -r ".name" $config_file )"
 
 
-xcodebuild -project ../taz.neo.xcodeproj/ -scheme "taz.neo" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 12' test  
+if [ "$platform" = "iOS Simulator" ]
+then
+    xcodebuild \
+        -project ../taz.neo.xcodeproj/ \
+        -scheme "taz.neo" \
+        -sdk iphonesimulator \
+        -destination "platform=iOS Simulator,name=${device_name}"\
+        clean test
+elif [ "$platform" = 'iOS' ] 
+then
+    xcodebuild \
+            -project ../taz.neo.xcodeproj/ \
+            -scheme "taz.neo" \
+            -destination "platform=iOS,name=${device_name}"\
+            clean test
+else    
+    echo "Platform must bei one of 'iOS Simluator' or 'iOS'"
+fi
